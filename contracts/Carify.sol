608839a5bbdfa16652dade22b4efad77e01b4f87
muaadhhd <muaadhhd@gmail.com>
2023-04-02 17:12:55 -0400

    Backend (contract) Finished

---
+++ contracts/Carify.sol
@@ -0,0 +1,112 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+// Uncomment this line to use console.log
+import "hardhat/console.sol";
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract Carify is ERC721, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter public _tokenIdCounter;
+
+    uint256 public maxSpots;
+    uint256 public price;
+
+    struct ParkingPass {
+        uint256 id;
+        uint256 expirationDate;
+        string licensePlate;
+        bool isOwned;
+    }
+
+    mapping (string  => ParkingPass) public passHolder;
+    mapping(string => bool) private registeredLicensePlates;
+
+
+    constructor(string memory _name, string memory _symbol, uint256 _maxSpots, uint256 _price) 
+        ERC721(_name, _symbol) 
+    {
+        maxSpots = _maxSpots;
+        price = _price;
+    }
+
+    function buyPass(string memory _licensePlate) public payable returns (uint256)
+    {
+        require(!isFull(), "Parking Lot is Full");
+        require(!isOwned(_licensePlate), "Already Owned");
+        require(msg.value >= price, "Amount is not sufficient");
+
+        uint256 newTokenId = _tokenIdCounter.current() + 1;
+
+        ParkingPass memory pass = ParkingPass(newTokenId, block.timestamp + 30, _licensePlate, true);
+
+        passHolder[_licensePlate] = pass;
+
+        _safeMint(msg.sender, newTokenId);
+
+        _tokenIdCounter.increment();
+
+        passHolder[_licensePlate].isOwned = true;
+
+        return newTokenId;
+    }
+
+    function renew(string memory _licensePlate) public payable
+    {
+        ParkingPass storage pass = passHolder[_licensePlate];
+
+        require(pass.isOwned, "Pass does not exist");
+        require(_exists(pass.id),"This parking pass does not exist.");
+        require(msg.value >= price, "Renewal fee is 0.01 ETH");
+
+        pass.expirationDate += 30 days;
+         
+    }
+
+    function cancelPass(string memory _licensePlate, uint256 _tokenId) public 
+    {
+
+        require(msg.sender == ownerOf(_tokenId), "Not the owner of the pass");
+        require(isOwned(_licensePlate), "This parking pass does not exist.");
+
+        delete passHolder[_licensePlate];
+        _burn(_tokenId);
+    }
+
+
+    function getPass(string memory _licensePlate) public view returns (ParkingPass memory)
+    {
+        return passHolder[_licensePlate];
+    }
+
+    function isFull() public view returns (bool) 
+    {
+        return _tokenIdCounter.current() >= maxSpots;
+        
+    }
+
+    function isOwned(string memory _licensePlate) public view returns (bool) 
+    {
+        return passHolder[_licensePlate].isOwned;
+    }
+
+    function isPassValid(string memory _licensePlate) public view returns (bool)
+    {
+        return
+            passHolder[_licensePlate].expirationDate > block.timestamp;
+    }
+
+    function transferPass(address newOwner, uint256 tokenId) public {
+        require(msg.sender == ownerOf(tokenId), "Not the owner");
+        _transfer(msg.sender, newOwner, tokenId);
+    }
+ 
+    function withdraw() public onlyOwner 
+    {
+        payable(owner()).transfer(address(this).balance);
+    }
+}
---
+++ test/Carify.js
@@ -0,0 +1,173 @@
+const {
+  time,
+  loadFixture,
+} = require("@nomicfoundation/hardhat-network-helpers");
+const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
+const { errors } = require("ethers");
+const chai = require('chai')
+  , expect = chai.expect
+  , should = chai.should();
+
+const tokens = (n) => {
+  return ethers.utils.parseUnits(n.toString(), 'ether')
+}
+
+describe("Carify", function () {
+  let deployer, user, user2
+  let carify
+
+  const NAME = "Carify"
+  const SYMBOL = "PASS"
+  const AMOUNT = tokens(0.01)
+  const spots = 100
+
+  beforeEach(async () => {
+    // Setup accounts
+    [deployer, user, user2] = await ethers.getSigners()
+
+    // Deploy contract
+    const Carify = await ethers.getContractFactory("Carify")
+    carify = await Carify.deploy(NAME, SYMBOL, spots, AMOUNT)
+
+  })
+
+  describe("Deployment", function () {
+    it("Sets the name", async () => {
+      const result = await carify.name()
+      expect(result).to.equal(NAME)
+    })
+
+    it("Sets the symbol and owner", async () => {
+      const result = await carify.symbol()
+      expect(result).to.equal(SYMBOL)
+
+      const result2 = await carify.owner()
+      expect(result2).to.equal(deployer.address)
+    })
+
+    it("Sets the price", async () => {
+      const result = await carify.price()
+      expect(result).to.equal(AMOUNT)
+    })
+
+    it('Returns total spots', async () => {
+      const result = await carify.maxSpots()
+      expect(result).to.be.equal(spots)
+    })
+  })
+
+  describe("Pass Holders", () => {
+
+    var date = new Date(new Date().setDate(new Date().getDate() + 30));
+
+    beforeEach(async () => {
+      const transaction = await carify.connect(user).buyPass("CRJB976", { value: AMOUNT })
+      await transaction.wait()
+    })
+
+    it('Returns total passHolders', async () => {
+      const result = await carify._tokenIdCounter()
+      expect(result).to.be.equal(1)
+    })
+
+    it('Returns Pass Holder info', async () => {
+      const pass = await carify.getPass("CRJB976")
+      expect(pass.id).to.be.equal(1)
+      expect(pass.licensePlate).to.be.equal('CRJB976')
+      // expect(pass.expirationDate).to.be.equal(date.getTime)
+      expect(pass.isOwned).to.be.equal(true)
+    })
+
+    it('Checks if pass is owned', async () => {
+      const result = await carify.isOwned("CRJB976")
+      expect(result).to.equal(true)
+    })
+
+    it('Checks if pass is valid', async () => {
+      const result = await carify.isPassValid("CRJB976")
+      expect(result).to.equal(true)
+    })
+
+    it('Checks if Parking Lot is full', async () => {
+      const result = await carify.isFull()
+      expect(result).to.equal(false)
+
+    })
+
+    it("Renewing pass", async () => {
+      const result = await carify.renew("CRJB976", { value: AMOUNT })
+      await result.wait()
+    })
+
+    it("Canceling pass by owner of token", async () => {
+      const buy = await carify.connect(user).buyPass("CRJB977", { value: AMOUNT })
+      const pass = await carify.getPass("CRJB976")
+
+      // Cancel the pass
+      const result = await carify.connect(user).cancelPass("CRJB976", pass.id);
+      const passExists = await carify.isOwned("CRJB976");
+      expect(passExists).to.be.false;
+
+    })
+  })
+
+  describe("Transfering Ownership", () => {
+
+    beforeEach(async () => {
+      const transaction = await carify.connect(user).buyPass("CRJB976", { value: AMOUNT })
+      await transaction.wait()
+
+    })
+
+    it("Transfers pass to different owner", async () => {
+      const pass = await carify.getPass("CRJB976");
+      await carify.connect(user).transferPass(user2.address, pass.id)
+
+      expect(await carify.ownerOf(pass.id)).to.equal(user2.address);
+    })
+  })
+
+  describe("Withdrawing", () => {
+    let balanceBefore
+
+    beforeEach(async () => {
+      const transaction = await carify.connect(user).buyPass("CRJB976", { value: AMOUNT })
+      await transaction.wait()
+
+      const result = await carify.connect(user).renew("CRJB976", { value: AMOUNT })
+      await result.wait()
+
+      balanceBefore = await ethers.provider.getBalance(deployer.address)
+
+    })
+
+    it("Getting balance of contract", async () => {
+      const balance = await ethers.provider.getBalance(carify.address);
+      let finalBalance = balance
+      console.log(`${finalBalance} ETH`)
+
+    })
+
+    it('Updates the owner balance', async () => {
+
+      transaction = await carify.connect(deployer).withdraw()
+      await transaction.wait()
+
+      const balanceAfter = await ethers.provider.getBalance(deployer.address)
+
+      let finalBalance = balanceAfter
+      console.log(`${finalBalance} ETH`)
+      expect(balanceAfter).to.be.greaterThan(balanceBefore)
+    })
+
+    it('Updates the contract balance', async () => {
+      transaction = await carify.connect(deployer).withdraw()
+      await transaction.wait()
+
+      const result = await ethers.provider.getBalance(carify.address)
+      let finalBalance = result
+      console.log(`${finalBalance} ETH`)
+      expect(result).to.equal(0)
+    })
+  })
+})
